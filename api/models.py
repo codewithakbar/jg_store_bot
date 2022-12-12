f44ef668f3f897abeba5da9bddf9372eaec0dfa5
AkbarAli <88106359+codewithakbar@users.noreply.github.com>
2022-12-12 23:51:18 +0500

    Product admin site register

--- api/models.py
+++ api/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    image = models.ImageField()
+    title = models.TextField()
+    full_description = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+    
