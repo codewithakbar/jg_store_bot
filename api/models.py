75c979202c1c535bc5cbe7b764af52d737548b5d
AkbarAli <88106359+codewithakbar@users.noreply.github.com>
2022-12-13 00:14:54 +0500

    api restfamework urls

--- api/models.py
+++ api/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 
 
+class User(models.Model):
+    username = models.CharField(max_length=233)
+    url = models.URLField(max_length=211)
+    user_id = models.IntegerField(max_length=44)
+
+
 class Product(models.Model):
     image = models.ImageField()
     title = models.TextField()
--- jg_store_bot/settings.py
+++ jg_store_bot/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'rest_framework',
+
     'api',
 ]
 
@@ -79,6 +82,15 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
+}
+
+
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
 }
 
 
