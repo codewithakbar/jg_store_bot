b73c32ff94df6dd3ad5fcd3197963026c7ede777
AkbarAli <88106359+codewithakbar@users.noreply.github.com>
2022-12-25 22:32:30 +0500

    cors host any

--- jg_store_bot/settings.py
+++ jg_store_bot/settings.py
@@ -62,10 +62,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ORIGIN_WHITELIST = (
-    'http://localhost:3000',
-    'http://localhost:8000',
-)
+CORS_ORIGIN_ALLOW_ALL = True
 
 
 ROOT_URLCONF = 'jg_store_bot.urls'
